22512466128530d6f199e8ee5df79accb154219f
MeiLin <postget.me@gmail.com>
2015-01-10 23:00:15 +0800

    done

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = myexpress;
+
+function myexpress() {
+  function app(req, res, next) {
+    res.statusCode = 404;
+    res.end();
+  }
+  
+  app.listen = function (port, done) {
+    var http = require("http");
+    var server = http.createServer(app);
+    server.listen(port, done);
+    return server;
+  }
+
+  return app;
+}
---
+++ test/app_spec.js
@@ -0,0 +1,32 @@
+var request = require("supertest")
+  , expect = require("chai").expect
+  , http = require("http");
+
+var express = require("../");
+
+describe("Implement Empty App",function() {
+  var app = express();
+  describe("as handler to http.createServer",function() {
+    it("responds to /foo with 404", function(done) {
+      var server = http.createServer(app);
+      request(server).get("/foo").expect(404).end(done);
+    });
+  });
+
+  describe("Defining the app.listen method",function() {
+    var port = 7001;
+    var server;
+
+    before(function(done) {
+      server = app.listen(port,done);
+    });
+
+    it("should return an http.Server",function() {
+      expect(server).to.be.instanceof(http.Server);
+    });
+
+    it("responds to /foo with 404",function(done) {
+      request("http://localhost:" + port).get("/foo").expect(404).end(done);
+    })
+  });
+});
